029f2e109a2ea8739c644c05c0f4d3cf7694cd3b
dbores <dbores@global.unam.mx>
2022-08-22 19:47:54 -0500

    Add cart functionalities

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Button, Card } from "react-bootstrap"
+import { useShoppingCart } from "../context/ShoppingCartContext"
 import { formatCurrency } from "../utilities/formatCurrency"
 
 type StoreItemProps = {
@@ -8,7 +9,15 @@ type StoreItemProps = {
     imgUrl: string
 }
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
-    const quantity = 0
+    const {
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart
+    } = useShoppingCart()
+    
+    const quantity = getItemQuantity(id)
+
     return (
         <>
             <Card className="h-100">
@@ -26,16 +35,22 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
                 </Card.Title>
                 <div className="mt-auto">
                     {quantity === 0 ? (
-                        <Button className="w-100">+ Add To Cart</Button>
-                    ) : <div className="d-flex align-items-center flex-column" style={{ gap: ".5rem"}}>
-                        <div className="d-flex align-items-center justify-content-center" style={{ gap: ".5rem"}}>
-                            <Button>-</Button>
+                        <Button className="w-100" onClick={() => increaseCartQuantity(id)}>+ Add To Cart</Button>
+                    ) : <div 
+                            className="d-flex align-items-center flex-column" 
+                            style={{ gap: ".5rem"}}
+                        >
+                        <div 
+                            className="d-flex align-items-center justify-content-center" 
+                            style={{ gap: ".5rem"}}
+                        >
+                            <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
                             <div>
                                 <span className="fs-3">{quantity}</span> In cart
                             </div>
-                            <Button>+</Button>
+                            <Button onClick={() => increaseCartQuantity(id)}>+</Button>
                         </div>
-                        <Button variant="danger" size="sm">
+                        <Button onClick={() => removeFromCart(id)} variant="danger" size="sm">
                             Remove
                         </Button>
                         </div>}
